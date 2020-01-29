553d64f46e4a5ca5c9567de28c025ac286260777
NolanKoblischke <57974254+NolanKoblischke@users.noreply.github.com>
2020-01-28 19:09:07 -0800

    Add files via upload

---
+++ BME280.ino
@@ -0,0 +1,41 @@
+#include <Seeed_BME280.h>
+#Link to Library: https://github.com/Seeed-Studio/Grove_BME280
+#include "Seeed_BME280.h"
+#include <Wire.h>
+
+BME280 bme280;
+
+void setup()
+{
+  Serial.begin(9600);
+  if(!bme280.init()){
+    Serial.println("Device error!");
+  }
+}
+
+void loop()
+{
+  float pressure;
+  
+  //get and print temperatures
+  Serial.print("Temp: ");
+  Serial.print(bme280.getTemperature());
+  Serial.println("C");//The unit for  Celsius because original arduino don't support special symbols
+  
+  //get and print atmospheric pressure data
+  Serial.print("Pressure: ");
+  Serial.print(pressure = bme280.getPressure());
+  Serial.println("Pa");
+
+  //get and print altitude data
+  Serial.print("Altitude: ");
+  Serial.print(bme280.calcAltitude(pressure));
+  Serial.println("m");
+
+  //get and print humidity data
+  Serial.print("Humidity: ");
+  Serial.print(bme280.getHumidity());
+  Serial.println("%");
+
+  delay(1000);
+}
